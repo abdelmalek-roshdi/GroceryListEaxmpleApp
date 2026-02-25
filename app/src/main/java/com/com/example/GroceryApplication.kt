ac64c2a364001c1462cfe3ee9bec2a027176d043
Abdelmalek Gamal <malekroshdi@gmail.com>
2026-02-25 19:58:42 +0200

    moving app class

--- app/src/main/java/com/com/example/GroceryApplication.kt
+++ app/src/main/java/com/com/example/GroceryApplication.kt
@@ -1,8 +1,7 @@
-package com.example
+package com.com.example
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class GroceryApplication : Application()
-
